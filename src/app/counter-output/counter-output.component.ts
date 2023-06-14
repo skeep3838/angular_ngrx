f9ba33137cad50f254da419f9642b72a8e3f22a9
林佳慧 <nicole.lin@webcomm.com.rw>
2023-06-14 18:01:01 +0800

    0-移除不必要的參數

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -11,23 +11,11 @@ import { CounterService } from '../counter.service';
 })
 export class CounterOutputComponent {
   // 如果此屬性最後除了一個observable，則可以在後面加$
-  count$ : Observable<number>;
+  count$: Observable<number>;
 
-  constructor(private counterService: CounterService, 
-              private store: Store<{counter: number}>) {
-                // 從store讀取數據，因為是Observable，所以數據發生變化會自動更新
-                this.count$ = store.select('counter');
-              }
+  constructor(private store: Store<{ counter: number }>) {
+    // 從store讀取數據，因為是Observable，所以數據發生變化會自動更新
+    this.count$ = store.select('counter');
+  }
 
-  // ngOnInit(): void {
-  //   this.counterServiceSub = this.counterService.counterChanged.subscribe(
-  //     (newVal) => (this.counter = newVal)
-  //   );
-  // }
-
-  // ngOnDestroy(): void {
-  //   if (this.counterServiceSub) {
-  //     this.counterServiceSub.unsubscribe();
-  //   }
-  // }
 }
